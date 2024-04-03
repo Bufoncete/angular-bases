9c348a604bc3a67ef8b2ee2a1c54d9003f7c39d4
Eduardo <ecmartinez@teralco.com>
2024-04-03 17:07:49 +0200

    72 a 75 Paquetes externos y servicio privado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -9,7 +9,18 @@ import { DbzService } from '../sevices/dbz.service';
 
 export class MainPageComponent {
 
-  constructor( public dbzService: DbzService){
-    
+  constructor( private dbzService: DbzService){
+  }
+
+  get characters(): Character[]{
+    return [ ...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id:string):void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(charater:Character):void {
+    this.dbzService.addCharacter(charater);
   }
 }
