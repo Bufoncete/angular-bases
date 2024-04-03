b64efc2ec748c687f7c42dbf7229c4e272e3767e
Eduardo <ecmartinez@teralco.com>
2024-04-03 12:18:02 +0200

    63 @input() Recibir del padre

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,3 +1,4 @@
+import { Character } from './../interfaces/character.interface';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
+  public characters:Character []=[{
+    name:'Krillin',
+    power:1000
+  },{
+    name:'Goku',
+    power:9500
+  },{
+    name:'Vegeta',
+    power:7500
+  }];
 
 }
