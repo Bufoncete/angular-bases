598cc9b112d9d654fd63c9d85bf1195eabb53b92
Eduardo <ecmartinez@teralco.com>
2024-04-03 16:06:20 +0200

    69 y 70 añadir personaje al listado y solución

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -11,5 +11,13 @@ export class ListComponent {
   public characterList: Character[]=[{
     name: 'Trunks',
     power: 10
-  }]
+  }];
+  //onDelete=Index value:number
+  @Output()
+  public onDelete:EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index:number):void{
+   this.onDelete.emit(index);
+
+  }
 }
