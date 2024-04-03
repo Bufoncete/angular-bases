a7eab1fd0ba46c003494b20329513d26117e53c4
Eduardo <ecmartinez@teralco.com>
2024-04-03 13:51:24 +0200

    67 @Output() - Emitir eventos al padre

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,5 @@
-import { Character } from './../interfaces/character.interface';
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,15 +7,19 @@ import { Component } from '@angular/core';
 })
 
 export class MainPageComponent {
-  public characters:Character []=[{
-    name:'Krillin',
-    power:1000
-  },{
-    name:'Goku',
-    power:9500
-  },{
-    name:'Vegeta',
-    power:7500
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  }, {
+    name: 'Goku',
+    power: 9500
+  }, {
+    name: 'Vegeta',
+    power: 7500
   }];
 
+  onNewCharacter(character: Character): void {
+    console.log('MainPage');
+    console.log(character);
+  }
 }
